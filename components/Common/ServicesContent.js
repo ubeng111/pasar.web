0762be3c61e18a029bc74ae6691df26470178e50
ubeng111 <152304955+ubeng111@users.noreply.github.com>
2024-11-23 08:18:51 +0700

    Update ServicesContent.js

--- components/Common/ServicesContent.js
+++ components/Common/ServicesContent.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 
 const ServicesContent = () => {
   return (
@@ -17,9 +16,7 @@ const ServicesContent = () => {
                 <div className="icon bg-faddd4">
                   <i className="flaticon-landing-page"></i>
                 </div>
-                <h3>
-                  <Link href="/single-service/">Optimisasi Mesin Pencari (SEO)</Link>
-                </h3>
+                <h3>Optimisasi Mesin Pencari (SEO)</h3>
                 <p>
                   Kami membantu Anda meningkatkan peringkat website Anda di mesin pencari melalui strategi SEO yang teruji dan efektif.
                 </p>
@@ -31,9 +28,7 @@ const ServicesContent = () => {
                 <div className="icon bg-cafbf1">
                   <i className="flaticon-goal"></i>
                 </div>
-                <h3>
-                  <Link href="/single-service/">Strategi Media Sosial</Link>
-                </h3>
+                <h3>Strategi Media Sosial</h3>
                 <p>
                   Meningkatkan kehadiran brand Anda melalui strategi media sosial yang tepat dan efektif untuk menarik audiens yang lebih luas.
                 </p>
@@ -45,9 +40,7 @@ const ServicesContent = () => {
                 <div className="icon bg-ddd5fb">
                   <i className="flaticon-contract"></i>
                 </div>
-                <h3>
-                  <Link href="/single-service/">Analisis dan Data Real-Time</Link>
-                </h3>
+                <h3>Analisis dan Data Real-Time</h3>
                 <p>
                   Menggunakan data analitik untuk memantau dan mengevaluasi kinerja website Anda secara real-time, membantu Anda membuat keputusan yang lebih baik.
                 </p>
@@ -59,9 +52,7 @@ const ServicesContent = () => {
                 <div className="icon bg-fcdeee">
                   <i className="flaticon-application"></i>
                 </div>
-                <h3>
-                  <Link href="/single-service/">Manajemen Media Online</Link>
-                </h3>
+                <h3>Manajemen Media Online</h3>
                 <p>
                   Kami membantu mengelola dan memaksimalkan keberadaan online Anda dengan menggunakan berbagai platform media sosial untuk mencapai tujuan bisnis.
                 </p>
@@ -73,9 +64,7 @@ const ServicesContent = () => {
                 <div className="icon bg-c5ebf9">
                   <i className="flaticon-seo"></i>
                 </div>
-                <h3>
-                  <Link href="/single-service/">Pelaporan & Analisis</Link>
-                </h3>
+                <h3>Pelaporan & Analisis</h3>
                 <p>
                   Menyediakan laporan dan analisis mendalam untuk memantau kinerja SEO dan strategi pemasaran Anda agar dapat disesuaikan dengan tujuan bisnis Anda.
                 </p>
@@ -87,9 +76,7 @@ const ServicesContent = () => {
                 <div className="icon bg-f8fbd5">
                   <i className="flaticon-data-recovery"></i>
                 </div>
-                <h3>
-                  <Link href="/single-service/">Pemulihan dari Penalti</Link>
-                </h3>
+                <h3>Pemulihan dari Penalti</h3>
                 <p>
                   Kami membantu Anda untuk mengatasi penalti dari mesin pencari dan memulihkan peringkat website Anda dengan teknik SEO yang tepat.
                 </p>
